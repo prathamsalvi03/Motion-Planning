f1d3a9e5a7a90cc348f3e500fcee20613691bf4f
Pratham Salvi <122139180+prathamsalvi03@users.noreply.github.com>
2023-03-26 18:20:06 +0530

    Update QRRT.py

--- QRRT.py
+++ QRRT.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import math
 import sys
 import matplotlib.pyplot as plt
 import pathlib
@@ -11,63 +10,62 @@ sys.path.append(str(pathlib.Path(__file__).parent.parent))
 
 
 
+def main():
+        
 
 
 
+        class Parent def: 
+                planQRRTStar < planRRTStar
 
-#classdef planQRRTStar < planRRTStar
+                properties
+                ParentDepthSearch
+                ParentDepthRewire
+                properties(Access = protected)
+                ParentNodeIds
 
- #   properties
-  #      ParentDepthSearch
-    #    ParentDepthRewire
-    #end
-    #properties(Access = protected)
-     #   ParentNodeIds
-    #end
-    
-class methods:
+        class methods:
 
-        def __init__(self,stateSpace,stateVlidator)
-        super().__init__(stateSpace, stateValidator)
-        self.ParentDepthSearch = 2;
-        self.ParentDepthRewire = 1;
-        #self.ParentNodeIds = containers.Map('KeyType', 'double', 'ValueType', 'double');
+                def __init__(self,stateSpace,stateVlidator):
+                        class  super(): __init__(stateSpace, stateValidator)
+                        self.ParentDepthSearch = 2;
+                        self.ParentDepthRewire = 1;
+                        self.ParentNodeIds = containers.Map('KeyType', 'double', 'ValueType', 'double');
 
 
 
-        #def [newNodeId, statusCode, treeInternal] = extend(self, randState, treeInternal)
-         #   statusCode = self.InProgress;
-          #  newNodeId = nan;
-           # 
-            #idx = treeInternal.nearestNeighbor(randState);
-            #nearestNode = treeInternal.getNodeState(idx);
-            #costNN = treeInternal.getNodeCostFromRoot(idx);
+        def [newNodeId, statusCode, treeInternal] = extend(self, randState, treeInternal)
+        statusCode = self.InProgress;
+        newNodeId = nan; 
+        idx = treeInternal.nearestNeighbor(randState);
+        nearestNode = treeInternal.getNodeState(idx);
+        costNN = treeInternal.getNodeCostFromRoot(idx);
             
-            #d = self.StateSpace.distance(nearestNode, randState);
+        d = self.StateSpace.distance(nearestNode, randState);
 
 
-    def planning(self,animation = True):
+def planning(self,animation = True):
 
         self.node_list = [self.start]
-        if d < 1e-10
+        if d < 1e-10 :
                 statusCode = self.ExistingState;
                 return;
             
-            newState = randState;
+        newState = randState;
 
-            if d > self.MaxConnectionDistance
+        if d > self.MaxConnectionDistance:
                 newState = self.StateSpace.interpolate(nearestNode, newState, self.MaxConnectionDistance/d);
                 d = self.MaxConnectionDistance;
             
-            costNew = costNN + d;
+        costNew = costNN + d;
             
-            if self.StateValidator.isMotionValid(nearestNode, newState)
+        if self.StateValidator.isMotionValid(nearestNode, newState):
                 statusCode = self.MotionInCollision;
                 return;
             
-    def choose_parent(self , new_node , near_node):
-        if not nearIndices:
-            return None
+        def choose_parent(self , new_node , near_node):
+                if not nearIndices:
+                return None
         nearIndices = treeInternal.near(newState);
         costMin = costNew;
         idMin = -1;
@@ -80,7 +78,7 @@ class methods:
 
         costTentative = costNear + self.StateSpace.distance(stateNear, newState);
 
-        if costMin > costTentative & self.StateValidator.isMotionValid(stateNear, newState)
+        if costMin > costTentative & self.StateValidator.isMotionValid(stateNear, newState):
                     costMin = costTentative;
                     idMin = idNear;
             
@@ -94,21 +92,21 @@ class methods:
             
             
         for n in ParentDepthSearch : 
-            if self.ParentNodeIds.isKey(idMin)
-            idParent = self.ParentNodeIds(idMin);
-            stateParent = self.treeInternal.getNodeState(idParent);
-            costParent = self.treeInternal.getNodeCostFromRoot(idParent);
+            if self.ParentNodeIds.isKey(idMin):
+                idParent = self.ParentNodeIds(idMin);
+                stateParent = self.treeInternal.getNodeState(idParent);
+                costParent = self.treeInternal.getNodeCostFromRoot(idParent);
              #return       
-            costTentativeParent = costParent + self.StateSpace.distance(stateParent, newState);
-            if costMin > costTentativeParent & self.StateValidator.isMotionValid(stateParent, newState)
+                costTentativeParent = costParent + self.StateSpace.distance(stateParent, newState);
+            if costMin > costTentativeParent & self.StateValidator.isMotionValid(stateParent, newState):
         
-            costMin = costTentativeParent;
-            idMin = idParent;
+                costMin = costTentativeParent;
+                idMin = idParent;
             #return
 
         idNew = treeInternal.insertNode(newState, idMin);
-            self.ParentNodeIds(idNew) = idMin;
-         newNodeId = idNew;
+        self.ParentNodeIds(idNew) = idMin;
+        newNodeId = idNew;
 
         for k in nearIndices :
                 idNear = self.nearIndices(k);
@@ -117,16 +115,16 @@ class methods:
                 
                 idNew = newNodeId;
 
-                if costNear > costNew + self.StateSpace.distance(stateNear, newState) && self.StateValidator.isMotionValid(stateNear, newState)
-                    for l in.ParentDepthRewire
-                       if self.ParentNodeIds.isKey(idNew)
+                if costNear > costNew + self.StateSpace.distance(stateNear, newState) & self.StateValidator.isMotionValid(stateNear, newState):
+                    for l in ParentDepthRewire:
+                       if self.ParentNodeIds.isKey(idNew):
                              idParentRewire = self.ParentNodeIds(idNew);
                              stateParentRewire = treeInternal.getNodeState(idParentRewire);
                              costParentRewire = treeInternal.getNodeCostFromRoot(idParentRewire);
  
                              costTentativeParentRewire = costParentRewire + self.StateSpace.distance(stateNear, stateParentRewire);
  
-                             if costNear > costTentativeParentRewire && self.StateValidator.isMotionValid(stateNear, stateParentRewire)
+                             if costNear > costTentativeParentRewire & self.StateValidator.isMotionValid(stateNear, stateParentRewire):
                                  costNear = costTentativeParentRewire;
                                  idNew = idParentRewire;
                             #return 
@@ -135,17 +133,17 @@ class methods:
                     self.ParentNodeIds(idNear) = idNew;
 
                 
-        if self.GoalReachedFcn(self, self.CurrentGoalState, newState)
-                if self.ContinueAfterGoalReached
+        if self.GoalReachedFcn(self, self.CurrentGoalState, newState):
+                if self.ContinueAfterGoalReached:
                     statusCode = self.GoalReachedButContinue;
-                else
+                else:
                     statusCode = self.GoalReached;
                 
                 return;
             
             
-            if newNodeId == self.MaxNumTreeNodes
-                statusCode = obj.MaxNumTreeNodesReached;
+        if newNodeId == self.MaxNumTreeNodes:
+                statusCode = self.MaxNumTreeNodesReached;
                 return;
 
 
